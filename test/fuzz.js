c7383769850a931441b7267d49784e7a1a8464ee
Yi Li <yi_li@ntu.edu.sg>
2018-12-18 01:08:55 +0800

    Refactoring contract fuzzing code: extracting two functions for call sequence and inputs generation.

--- test/fuzz.js
+++ test/fuzz.js
@@ -6,22 +6,24 @@ var SimpleDAO = artifacts.require("SimpleDAO");
 var AttackDAO = artifacts.require("AttackDAO");
 
 contract('SimpleDAO', function(accounts) {
-
+  var dao;
+  var att;
+  
+  SimpleDAO.deployed().then(function(ins) { dao = ins; });
+  AttackDAO.deployed().then(function(ins) { att = ins; });
+  
   it("Fuzz SimpleDAO contract", async () => {
-    let dao = await SimpleDAO.deployed();
-    let att = await AttackDAO.deployed();
-    let att_con = await new web3.eth.Contract(att.abi, att.address);
-    
+    //let att_con = new web3.eth.Contract(att.abi, att.address);
     let balance = await web3.eth.getBalance(att.address);
     let balance_before = balance;
 
     // Generate call sequence
-    let payload1 = web3.eth.abi.encodeFunctionCall(att.abi[3], [att.address,
-								web3.utils.toWei('1', 'ether')]);
-    let payload2 = web3.eth.abi.encodeFunctionCall(att.abi[4], [web3.utils.toWei('1', 'ether')]);
+    //let payload1 = web3.eth.abi.encodeFunctionCall(att.abi[3], [att.address,
+		//						web3.utils.toWei('1', 'ether')]);
+    //let payload2 = web3.eth.abi.encodeFunctionCall(att.abi[4], [web3.utils.toWei('1', 'ether')]);
+    let sequence = await generateCallSequence(att.abi);
     
-    let sequence = [payload1, payload2];
-    
+    // Execute call sequence
     while (sequence.length > 0) {
       let payload = sequence.shift();
       await web3.eth.sendTransaction({ to: att.address,
@@ -39,7 +41,42 @@ contract('SimpleDAO', function(accounts) {
     balance = await web3.eth.getBalance(att.address);
     let balance_after = balance;
 
+    // Assert oracles
     assert.equal(balance_before, "2000000000000000000", "Initial balance");
     assert.equal(balance_after, "5000000000000000000", "Final balance");
   });
+
+  function generateFunctionInputs(abi) {
+    if (abi.constant) return;
+    if (abi.type != 'function') return;
+    
+    var parameters = [];
+    abi.inputs.forEach(function(param) {
+      if (param.type == 'address') {
+        parameters.push(att.address);
+      } else if (param.type == 'uint256') {
+        parameters.push(web3.utils.toWei('1', 'ether'));
+      } else {
+        // default parameter
+        parameters.push(0);
+      }
+    });
+    
+    return web3.eth.abi.encodeFunctionCall(abi, parameters);
+  }
+  
+  function generateCallSequence(abis) {
+    var calls = [];
+    
+    abis.forEach(function(abi) {
+      if (abi.constant || abi.type != 'function')
+        return;
+
+      if (abi.name == 'donate' || abi.name == 'withdraw')
+        calls.push(generateFunctionInputs(abi));
+    });
+    
+    return calls;
+  }
+  
 });
