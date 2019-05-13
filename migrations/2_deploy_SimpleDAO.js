8c2f878117f67e6557fefdcddb4ac2aa02567d74
palinatolmach <palina001@e.ntu.edu.sg>
2019-05-13 16:17:15 +0800

    Added gasless_send deployment

--- migrations/2_deploy_SimpleDAO.js
+++ migrations/2_deploy_SimpleDAO.js
@@ -1,15 +1,31 @@
 const SimpleDAO = artifacts.require("SimpleDAO"); 
 const Attack_SimpleDAO0 = artifacts.require("Attack_SimpleDAO0"); 
 const Attack_SimpleDAO1 = artifacts.require("Attack_SimpleDAO1"); 
-const Attack_SimpleDAO2 = artifacts.require("Attack_SimpleDAO2"); 
+const Attack_SimpleDAO2 = artifacts.require("Attack_SimpleDAO2");
 
-module.exports = function(deployer) { 
-	deployer.deploy(SimpleDAO) 
+const Adoption = artifacts.require("Adoption");
+const AdminInterface = artifacts.require("AdminInterface");
+const EtherTower = artifacts.require("EtherTower");
+const AmIOnTheFork = artifacts.require("AmIOnTheFork");
+const ReplaySafeSplit = artifacts.require("ReplaySafeSplit");
+const Etheramid = artifacts.require("Etheramid");
+
+
+module.exports = function(deployer) {
+	deployer.deploy(AmIOnTheFork).then(function() {
+		return deployer.deploy(ReplaySafeSplit, AmIOnTheFork.address)})
+
+	deployer.deploy(Adoption);
+	deployer.deploy(AdminInterface);
+	deployer.deploy(EtherTower);
+	deployer.deploy(Etheramid);
+
+	deployer.deploy(SimpleDAO)
 	.then(function() {
-		return deployer.deploy(Attack_SimpleDAO0, SimpleDAO.address, {value: web3.toWei(10000000000000, "ether")}); 
+		return deployer.deploy(Attack_SimpleDAO0, SimpleDAO.address, {value: 10e18});
 	}).then(function() {
-		return deployer.deploy(Attack_SimpleDAO1, SimpleDAO.address, {value: web3.toWei(10000000000000, "ether")}); 
+		return deployer.deploy(Attack_SimpleDAO1, SimpleDAO.address, {value: 10e18});
 	}).then(function() {
-		return deployer.deploy(Attack_SimpleDAO2, SimpleDAO.address, {value: web3.toWei(10000000000000, "ether")}); 
+		return deployer.deploy(Attack_SimpleDAO2, SimpleDAO.address, {value: 10e18});
 	});
 }
