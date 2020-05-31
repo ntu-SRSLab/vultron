616dbf1575b4812b572a461f81b3ebb5ffc4bb1d
liu <liu>
2020-05-31 15:51:33 +0800

    Finish  Test draft ui

--- app/src/components/Test copy.vue
+++ app/src/components/Test copy.vue
@@ -1,23 +1,53 @@
   <template>
-    <div>
-  <b-form inline>
-    <label class="sr-only" for="inline-form-input-name">Name</label>
-    <b-input
-      id="inline-form-input-name"
-      class="mb-2 mr-sm-2 mb-sm-0"
-      placeholder="Jane Doe"
-    ></b-input>
+    <div class>
+      <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+        <b-form-group
+          id="input-group-1"
+          label="Email address:"
+          label-for="input-1"
+          description="We'll never share your email with anyone else."
+        >
+          <b-form-input
+            id="input-1"
+            v-model="form.email"
+            type="email"
+            required
+            placeholder="Enter email"
+          ></b-form-input>
+        </b-form-group>
 
-    <label class="sr-only" for="inline-form-input-username">Username</label>
-    <b-input-group prepend="@" class="mb-2 mr-sm-2 mb-sm-0">
-      <b-input id="inline-form-input-username" placeholder="Username"></b-input>
-    </b-input-group>
+        <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
+          <b-form-input
+            id="input-2"
+            v-model="form.name"
+            required
+            placeholder="Enter name"
+          ></b-form-input>
+        </b-form-group>
 
-    <b-form-checkbox class="mb-2 mr-sm-2 mb-sm-0">Remember me</b-form-checkbox>
+        <b-form-group id="input-group-3" label="Food:" label-for="input-3">
+          <b-form-select
+            id="input-3"
+            v-model="form.food"
+            :options="foods"
+            required
+          ></b-form-select>
+        </b-form-group>
 
-    <b-button variant="primary">Save</b-button>
-  </b-form>
-</div>
+        <b-form-group id="input-group-4">
+          <b-form-checkbox-group v-model="form.checked" id="checkboxes-4">
+            <b-form-checkbox value="me">Check me out</b-form-checkbox>
+            <b-form-checkbox value="that">Check that out</b-form-checkbox>
+          </b-form-checkbox-group>
+        </b-form-group>
+
+        <b-button type="submit" variant="primary">Submit</b-button>
+        <b-button type="reset" variant="danger">Reset</b-button>
+      </b-form>
+      <b-card class="mt-3" header="Form Data Result">
+        <pre class="m-0">{{ form }}</pre>
+      </b-card>
+    </div>
   </template>
 
   <script>
