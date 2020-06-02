89b7a3138879aab05e7cb8152a56f4805da64ee5
liu <liu>
2020-06-02 16:04:36 +0800

    Get a client side of vultron

--- migrations/6_deploy_PIGGY_BANK.js
+++
@@ -1,7 +0,0 @@
-const PIGGY_BANK = artifacts.require("PIGGY_BANK"); 
-const Log = artifacts.require("Log"); 
-const Attack_PIGGY_BANK0 = artifacts.require("Attack_PIGGY_BANK0"); 
-const Attack_PIGGY_BANK1 = artifacts.require("Attack_PIGGY_BANK1"); 
-
-module.exports = function(deployer) { 
-}
--- utils/Notes.md
+++
@@ -1,3 +0,0 @@
-1. reentrancy: ProxyCreationAndExecute.sol is deleted, because its parameters are special address type
-2. reentrancy: WalletSimple in Forwarder.sol is deleted, because it construct needs a list of address 
- 
