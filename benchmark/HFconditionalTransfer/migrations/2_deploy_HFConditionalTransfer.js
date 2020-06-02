ad2e76c3833262e9996f7a6c24aefa23f43c8dfc
liu <liu>
2020-06-02 15:59:25 +0800

    Clean directory for client side of vultron

--- benchmark/HFconditionalTransfer/migrations/2_deploy_HFConditionalTransfer.js
+++
@@ -1,9 +0,0 @@
-const HFConditionalTransfer = artifacts.require("HFConditionalTransfer"); 
-const Attack_HFConditionalTransfer0 = artifacts.require("Attack_HFConditionalTransfer0"); 
-
-module.exports = function(deployer) { 
-	deployer.deploy(HFConditionalTransfer) 
-	.then(function() {
-		return deployer.deploy(Attack_HFConditionalTransfer0, HFConditionalTransfer.address, {value: web3.toWei(100000, "ether")}); 
-	});
-}
--- benchmark/transferIntwopart/migrations/2_deploy_TranferInTwoPart.js
+++
@@ -1,9 +0,0 @@
-const TranferInTwoPart = artifacts.require("TranferInTwoPart"); 
-const Attack_TranferInTwoPart0 = artifacts.require("Attack_TranferInTwoPart0"); 
-
-module.exports = function(deployer) { 
-	deployer.deploy(TranferInTwoPart) 
-	.then(function() {
-		return deployer.deploy(Attack_TranferInTwoPart0, TranferInTwoPart.address, {value: web3.toWei(100000, "ether")}); 
-	});
-}
