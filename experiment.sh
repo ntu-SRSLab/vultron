5345ce3b8e1bce65f5ffc571484e818019db2ec6
liu <liuye5@live.com>
2020-07-12 18:04:13 +0800

    save

--- experiment.sh
+++ experiment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 DEPLOY_TIMEOUT=2m
-TESTING_TIMEOUT=5m
+TESTING_TIMEOUT=3m
 mkdir  -p experimentlog
 for  benchmark in $(ls ./benchmark)
 do
@@ -24,27 +24,44 @@ do
                 fi
             fi
     done
-    echo $source, ${attacks[@]}
+    echo $benchmark" is the current benchmark! "
+    echo $source 
+    echo ${attacks[@]}
     for attack in ${attacks[@]}:
     do
+	    echo $benchmark" will be deployed to private network."
             nohup ./utils/startTruffle.sh >  experimentlog/$benchmark/deploy.log  2>&1 &
             sleep $DEPLOY_TIMEOUT
+	    # if deploy not done, wait another deploy timeout
+            if [[  $(grep "Saving artifacts..." experimentlog/$benchmark/deploy.log | wc -l) -lt 2 ]]; then
+            	sleep $DEPLOY_TIMEOUT
+	    fi
             if [[  $(grep "Saving artifacts..." experimentlog/$benchmark/deploy.log | wc -l) -eq 2 ]]; then
-                    # cat deploy.log
-                    echo $benchmark, " deploy successfully."
+                    echo $benchmark" deploy successfully."
+	    	    echo $benchmark" under testing..."
                     nohup node silent-server.js  $source  $attack > experimentlog/$benchmark/$attack.log 2>&1 &
                     sleep $TESTING_TIMEOUT
+		    # if cannot found violation error at the time, wait anothother testing_timeout.
+                    if [[  $(grep "seconds" experimentlog/$benchmark/$attack.log | wc -l) -eq 0 ]]; then
+                     	sleep $TESTING_TIMEOUT
+		    fi
+		    # collect results, whether there is violation error or not.
                     if [[  $(grep "seconds" experimentlog/$benchmark/$attack.log | wc -l) -eq 1 ]]; then
-                                    echo "$benchmark 's $attack found violation"
+			            echo "collect results..."
+                                    echo "${benchmark}'s $attack found a violation"
                                     echo $(grep "invariant" experimentlog/$benchmark/$attack.log)
                                     echo $(grep "seconds" experimentlog/$benchmark/$attack.log)
                                     echo "***************************"
                     fi
             else 
-                    echo "deploy failed"
+                    echo $benchmark" deployment failure."
             fi
-            echo $1 | sudo -S kill -9 $(pgrep node)
-            echo $1 | sudo -S kill -9 $(pgrep truffle)
+	    if [[ $(pgrep node| wc -l) -gt 0 ]]; then
+                echo $1 | sudo -S kill -9 $(pgrep node)
+    	    fi
+	    if [[ $(pgrep startTruffle| wc -l) -gt 0 ]]; then
+             	echo $1 | sudo -S kill -9 $(pgrep startTruffle)
+    	    fi
 	    sleep 10
 
     done
